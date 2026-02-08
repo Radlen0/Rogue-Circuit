580cdfa2d9a17cdc2665942595f9cfbaddf2c61a
Radlen <code.radlen@gmail.com>
2026-02-08 01:03:53 +0100

    feat(util):  implement SpriteStack2D for dynamic sprite layering and rotation

---
+++ src/systems/sprite_stack_2d/sprite_stack_2d.gd
@@ -0,0 +1,46 @@
+@tool
+extends Node2D
+class_name SpriteStack2D
+
+@export var texture_resource : SpriteFrames
+@export var offset : int = 1
+
+
+@export_tool_button("Create Stack") var create_stack = func () :
+	for child in get_children():
+		child.queue_free()
+	
+	if not texture_resource:
+		printerr("No SpriteFrames assigned!")
+		return
+		
+	for i in range(texture_resource.get_frame_count("default")):
+		var frame_texture = texture_resource.get_frame_texture("default", i)
+			
+		var sprite : Sprite2D = Sprite2D.new()
+		sprite.texture = frame_texture
+		sprite.position.y = -i * offset
+		add_child(sprite)
+		sprite.owner = get_tree().edited_scene_root
+		sprite.name = "Layer_" + str(i)
+
+@export_range(0, 360) var rotation_angle : float = 0:
+		set(value):
+			rotation_angle = value
+			update_rotation(deg_to_rad(rotation_angle))
+
+@export var face_camera : bool = true 
+
+func _process(_delta: float) -> void:
+	if Engine.is_editor_hint():
+		return
+	if face_camera:
+		var cam = get_viewport().get_camera_2d()
+	
+		global_rotation = cam.get_screen_rotation()
+	
+		update_rotation(deg_to_rad(rotation_angle) - global_rotation)
+
+func update_rotation(rotation_value: float) -> void:
+	for sprite in get_children():
+		sprite.rotation = rotation_value
